62f1483d3a3b7b7ec0288f08620d8bd9ae587e6f
Sean DMR <sean@flyinggrizzly.net>
2022-03-21 16:39:09 +0000

    Bootstrap packer inline

--- lib/nvim/lua/plugins.lua
+++ lib/nvim/lua/plugins.lua
@@ -1,6 +1,11 @@
-local packer = require 'lib/packer_init'
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
 
-packer.startup(function(use)
+return require('packer').startup(function(use)
+  -- My plugins here
   use { 'wbthomason/packer.nvim' } -- Let packer manage itself
 
   use { 'Shopify/shadowenv.vim' }
@@ -118,4 +123,9 @@ packer.startup(function(use)
   }
   use { 'weilbith/nvim-code-action-menu' }
 
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
