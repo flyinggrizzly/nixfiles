ebbcb95e966c3c40c34fc1e0a005cbde15e3ab21
Sean DMR <sean@flyinggrizzly.net>
2022-03-21 01:22:13 +0000

    Add missing keymaps

--- lib/nvim/lua/keymaps.lua
+++ lib/nvim/lua/keymaps.lua
@@ -5,7 +5,28 @@ vim.g.maplocalleader = ' '
 
 keymap('', 'gf', ':edit <cfile><CR>')
 
+
 vim.cmd [[
+  " zoom a vim pane, <C-w>= to re-balance
+  nnoremap <leader>- :wincmd _<cr>:wincmd \|<cr>
+  nnoremap <leader>= :wincmd =<cr>
+
+  " jk in rapid succession will bring you out of insert mode
+  inoremap jk <esc>
+  inoremap kj <esc>
+
+  cnoremap jk <c-c>
+  cnoremap kj <c-c>
+
+  vnoremap v <esc>
+
+  " Quick save shortcut
+  noremap <Leader>f :update<CR>
+
+  " Quick tab switching
+  nnoremap H gT
+  nnoremap L gt
+
   " Get off my lawn
   nnoremap <Left> :echoe "Use h"<CR>
   nnoremap <Right> :echoe "Use l"<CR>
