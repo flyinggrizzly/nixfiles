c6a50f43f0df7cba321a1209810936c1595eb073
Sean DMR <sean.moranrichards@shopify.com>
2022-07-11 10:08:08 +0100

    Add YankLastShaForLine git vim func

--- lib/nvim/lua/functions.lua
+++ lib/nvim/lua/functions.lua
@@ -13,3 +13,25 @@ vim.cmd [[
   :command! PromoteToLet :call PromoteToLet()
   :map <leader>p :PromoteToLet<cr><Paste>"
 ]]
+
+vim.cmd [[
+  """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+  " YANK LAST GIT SHA FOR LINE
+  """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+  func! YankLastShaForLine()
+    :Git blame
+    :normal! yiw
+
+    " Need to make blame buffer modifiable to be able to delete it
+    :set ma
+    :bdelete
+
+    :wincmd p
+  endfunc
+
+  " Workflow for quick fixups:
+  " Use ys to yank the last SHA for the current line
+  " Use yf to form a fixup command
+  nmap <leader>ys :call YankLastShaForLine()<CR>
+  nmap <leader>yf :Git commit --fixup=<c-r>0
+]]
