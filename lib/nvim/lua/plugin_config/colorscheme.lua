f8626f31af2ca976e485b858b2097ee95667ca3b
Sean DMR <sean.moranrichards@shopify.com>
2022-08-30 12:50:30 +0100

    Switch to light colorscheme

--- lib/nvim/lua/plugin_config/colorscheme.lua
+++ lib/nvim/lua/plugin_config/colorscheme.lua
@@ -1,9 +1,9 @@
 -- Vim-One
---vim.cmd [[
-  --set background=light
-  --let g:one_allow_italics = 1
-  --colorscheme one
---]]
+vim.cmd [[
+  set background=light
+  let g:one_allow_italics = 1
+  colorscheme one
+]]
 
 -- Solarized Light
 --vim.cmd [[
@@ -14,29 +14,16 @@
 
 -- Papercolor
 --vim.cmd [[
-  --set t_Co=256
 
   --set background=light
   --colorscheme PaperColor
 
-  --let g:PaperColor_Theme_Options = {
-  --\   'theme': {
-  --\     'default': {
-  --\       'allow_bold': 1
-  --\     },
-  --\     'default.light': {
-  --\       'override' : {
-  --\         'linenumber_fg' : ['#00afaf', '37']
-  --\       }
-  --\     }
-  --\   }
-  --\ }
 --]]
 
 -- Dracula
-vim.cmd [[
-  colorscheme dracula
+--vim.cmd [[
+  --colorscheme dracula
 
-  highlight Normal ctermbg=235
-]]
+  --highlight Normal ctermbg=235
+--]]
 
