906075245331a4c89251be160964dfde264cd52d
Sean DMR <sean@flyinggrizzly.net>
2024-02-17 17:03:41 +0000

    Set up Vim config and imports

---
+++ lib/nvim/lua/plugin_config/airline.lua
@@ -0,0 +1,9 @@
+vim.cmd [[
+  let g:airline_theme = 'dracula'
+  let g:airline_powerline_fonts = 1
+  let g:airline_skip_empty_sections = 1
+
+  let g:airline_section_z = '{%l,%c}'
+
+  highlight LineNr ctermfg=grey
+]]
---
+++ lib/nvim/lua/plugin_config/colorscheme.lua
@@ -0,0 +1,29 @@
+-- Vim-One
+vim.cmd [[
+  set background=light
+  let g:one_allow_italics = 1
+  colorscheme one
+]]
+
+-- Solarized Light
+--vim.cmd [[
+  --syntax enable
+  --set background=light
+  --colorscheme solarized
+--]]
+
+-- Papercolor
+--vim.cmd [[
+
+  --set background=light
+  --colorscheme PaperColor
+
+--]]
+
+-- Dracula
+--vim.cmd [[
+  --colorscheme dracula
+
+  --highlight Normal ctermbg=235
+--]]
+
