57864994cd5eff04f4846d319d82d2ac032a7bf1
Sean DMR <sean@flyinggrizzly.net>
2024-02-18 16:35:59 +0000

    Fix Brewfile envvar name

--- configs/brew.nix
+++ configs/brew.nix
@@ -3,6 +3,6 @@
   # NOTE: dependency for lib/zsh/functions/brew-up
   home.file.".Brewfile".source = ../lib/Brewfile;
   home.sessionVariables = {
-    HOMEBREW_BREWFILE = "$HOME/.Brewfile";
+    HOMEBREW_BUNDLE_FILE = "$HOME/.Brewfile";
   };
 }
