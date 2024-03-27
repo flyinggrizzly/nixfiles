539dafdfdb03f313a0439550524146cb04278b4f
Sean DMR <sean.moranrichards@shopify.com>
2024-03-27 14:30:40 +0000

    Append to zshrc instead of governing it

--- configs/terminal.nix
+++ configs/terminal.nix
@@ -38,8 +38,11 @@
   # TODO: consider migrating zshrc and zsh/* config to declarative definitions
   programs.zsh = {
     enable = true;
+    initExtra = ''
+      source $HOME/.zshrc.hm-managed
+    '';
   };
-  home.file.".zshrc".source = ../lib/zshrc;
+  home.file.".zshrc.hm-managed".source = ../lib/zshrc;
   home.file.".zsh" = {
     source = ../lib/zsh;
     recursive = true;
