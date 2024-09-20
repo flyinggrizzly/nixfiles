d5938974ce931bd2d0a9347604a004500404b1f5
Sean DMR <sean@flyinggrizzly.net>
2024-09-20 18:29:37 +0100

    Start importing kickstart.nvim config

--- lib/nvim/base_init.lua
+++ lib/nvim/base_init.lua
@@ -1,4 +1,8 @@
 require 'options'
+require 'autocmd'
 require 'keymaps'
 require 'functions'
 require 'plugin_config'
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = false
--- lib/nvim/lua/keymaps.lua
+++ lib/nvim/lua/keymaps.lua
@@ -28,8 +28,36 @@ vim.cmd [[
   nnoremap L gt
 
   " Get off my lawn
-  nnoremap <Left> :echoe "Use h"<CR>
-  nnoremap <Right> :echoe "Use l"<CR>
-  nnoremap <Up> :echoe "Use k"<CR>
-  nnoremap <Down> :echoe "Use j"<CR>
+  " nnoremap <Left> :echoe "Use h"<CR>
+  " nnoremap <Right> :echoe "Use l"<CR>
+  " nnoremap <Up> :echoe "Use k"<CR>
+  " nnoremap <Down> :echoe "Use j"<CR>
 ]]
+
+-- TIP: Disable arrow keys in normal mode
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+
+-- TODO add whichkey stuff here
