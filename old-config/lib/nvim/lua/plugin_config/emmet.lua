ccdf8c3472625f73b037a0eb81c710639da36b52
Sean DMR <sean@flyinggrizzly.net>
2024-09-21 10:05:03 +0100

    Delete old-config dir

--- old-config/lib/nvim/lua/plugin_config/emmet.lua
+++
@@ -1,7 +0,0 @@
-vim.cmd [[
-  let g:user_emmet_leader_key = '<C-Y>' " trigger completion with Ctrl-Y , <- don't forget the comma
-  let g:user_emmet_settings = {
-  \      'javascript.jsx' : { 'extends' : 'jsx' },
-  \      'javascript' : { 'extends' : 'jsx' },
-  \     }
-]]
