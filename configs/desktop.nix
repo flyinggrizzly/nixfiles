ea646808c42561d5bafffb2b7f3bfc557084a886
Sean DMR <sean@flyinggrizzly.net>
2024-09-23 12:14:48 +0100

    Set up Kitty with nix

--- configs/desktop.nix
+++ configs/desktop.nix
@@ -1,14 +1,12 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   home.packages = [
-    pkgs.alacritty
     pkgs.logseq
     pkgs.google-chrome
   ];
 
   programs = {
-    kitty.enable = true;
     vscode.enable = true;
   };
 }
--- configs/terminal.nix
+++ configs/terminal.nix
@@ -1,5 +1,8 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
+let
+  jet_brains = "JetBrainsMono";
+in
 {
   home.packages = [
     pkgs.powerline-fonts
@@ -22,9 +25,27 @@
 
     pkgs.nix-prefetch
     pkgs.nix-search-cli
+
+    pkgs.alacritty
+    pkgs.kitty-themes
   ];
 
   # TODO: try nerdfots and kitty config: https://discourse.nixos.org/t/setting-up-console-font-nerdfonts/37817/2
+  fonts.fontconfig.enable = true;
+  fonts.fontconfig.defaultFonts.monospace = [ jet_brains ];
+  programs.kitty = {
+    enable = true;
+    font = {
+      name = "JetBrainsMono Nerd Font";
+      size = 14;
+      package = (pkgs.nerdfonts.override { fonts = [ jet_brains ]; });
+    };
+    themeFile = "Afterglow";
+    settings = {
+      cursor_blink_interval = 0;
+      enable_audio_bell = "no";
+    };
+  };
 
   home.file.".ripgreprc".source = ../lib/ripgreprc;
   home.file.".tmux.conf".source = ../lib/tmux.conf;
@@ -32,7 +53,7 @@
     source = ../lib/tmuxinator;
     recursive = true;
   };
-  home.file.".kitty.conf".source = ../lib/kitty.conf;
+  #home.file.".kitty.conf".source = ../lib/kitty.conf;
   home.file.".aliases".source = ../lib/aliases;
   home.file.".bin" = {
     source = ../lib/bin;
@@ -49,5 +70,3 @@
     recursive = true;
   };
 }
-
-
