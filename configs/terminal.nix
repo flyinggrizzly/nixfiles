398f923781f61b298512ec4902e387019a9619e5
Sean DMR <sean@flyinggrizzly.net>
2024-09-23 13:51:43 +0100

    Improve terminal emulator fonts

--- configs/terminal.nix
+++ configs/terminal.nix
@@ -1,7 +1,14 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 let
-  jet_brains = "JetBrainsMono";
+  kitty_symbol_map =
+    "U+e000-U+e00a,U+ea60-U+ebeb,U+e0a0-U+e0c8,U+e0ca,U+e0cc-U+e0d7,U+e200-U+e2a9,U+e300-U+e3e3,U+e5fa-U+e6b1,U+e700-U+e7c5,U+ed00-U+efc1,U+f000-U+f2ff,U+f000-U+f2e0,U+f300-U+f372,U+f400-U+f533,U+f0001-U+f1af0";
+  jetbrains_mono_pkg = pkgs.jetbrains-mono;
+  jetbrains_mono_name = "JetBrains Mono";
+  nerdfont_jetbrains_mono_pkg = "JetBrainsMono";
+  nerdfont_jetbrains_mono_name = "JetBrains Mono Nerd Font";
+  nerdfont_symbols_only_pkg = "NerdFontsSymbolsOnly";
+  nerdfont_symbols_only_name = "Symbols Nerd Font";
 in
 {
   home.packages = [
@@ -26,24 +33,46 @@ in
     pkgs.nix-prefetch
     pkgs.nix-search-cli
 
+    jetbrains_mono_pkg
     pkgs.alacritty
     pkgs.kitty-themes
+
+    # Overrides by font: https://github.com/NixOS/nixpkgs/blob/8764d898c4f365d98ef77af140b32c6396eb4e02/pkgs/data/fonts/nerdfonts/shas.nix
+    (pkgs.nerdfonts.override { fonts = [ nerdfont_jetbrains_mono_pkg nerdfont_symbols_only_pkg ]; })
   ];
 
   # TODO: try nerdfots and kitty config: https://discourse.nixos.org/t/setting-up-console-font-nerdfonts/37817/2
   fonts.fontconfig.enable = true;
-  fonts.fontconfig.defaultFonts.monospace = [ jet_brains ];
+  fonts.fontconfig.defaultFonts.monospace = [ jetbrains_mono_name ];
+
   programs.kitty = {
     enable = true;
     font = {
-      name = "JetBrainsMono Nerd Font";
+      name = jetbrains_mono_name;
       size = 14;
-      package = (pkgs.nerdfonts.override { fonts = [ jet_brains ]; });
+      package = pkgs.jetbrains-mono;
     };
     themeFile = "Afterglow";
     settings = {
       cursor_blink_interval = 0;
       enable_audio_bell = "no";
+      # See https://sw.kovidgoyal.net/kitty/faq/#kitty-is-not-able-to-use-my-favorite-font
+    };
+    extraConfig = ''
+      symbol_map U+e000-U+e00a,U+ea60-U+ebeb,U+e0a0-U+e0c8,U+e0ca,U+e0cc-U+e0d4,U+e200-U+e2a9,U+e300-U+e3e3,U+e5fa-U+e6b1,U+e700-U+e7c5,U+f000-U+f2e0,U+f300-U+f372,U+f400-U+f532,U+f0001-U+f1af0 Symbols Nerd Font Mono
+    '';
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      font = {
+        size = 14;
+        normal = {
+          family = nerdfont_jetbrains_mono_name;
+          style = "Regular";
+        };
+      };
     };
   };
 
@@ -53,7 +82,6 @@ in
     source = ../lib/tmuxinator;
     recursive = true;
   };
-  #home.file.".kitty.conf".source = ../lib/kitty.conf;
   home.file.".aliases".source = ../lib/aliases;
   home.file.".bin" = {
     source = ../lib/bin;
