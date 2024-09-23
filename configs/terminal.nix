540ee63b0e86877ad62bc4b49f670d26ba717360
Sean DMR <sean@flyinggrizzly.net>
2024-09-23 15:04:16 +0100

    Better font overrides

--- configs/terminal.nix
+++ configs/terminal.nix
@@ -33,24 +33,23 @@ in
     pkgs.nix-prefetch
     pkgs.nix-search-cli
 
-    jetbrains_mono_pkg
     pkgs.alacritty
     pkgs.kitty-themes
 
-    # Overrides by font: https://github.com/NixOS/nixpkgs/blob/8764d898c4f365d98ef77af140b32c6396eb4e02/pkgs/data/fonts/nerdfonts/shas.nix
+    jetbrains_mono_pkg
     (pkgs.nerdfonts.override { fonts = [ nerdfont_jetbrains_mono_pkg nerdfont_symbols_only_pkg ]; })
   ];
 
   # TODO: try nerdfots and kitty config: https://discourse.nixos.org/t/setting-up-console-font-nerdfonts/37817/2
-  fonts.fontconfig.enable = true;
-  fonts.fontconfig.defaultFonts.monospace = [ jetbrains_mono_name ];
+  #fonts.fontconfig.enable = true;
+  #fonts.fontconfig.defaultFonts.monospace = [ jetbrains_mono_name ];
 
   programs.kitty = {
     enable = true;
     font = {
       name = jetbrains_mono_name;
       size = 14;
-      package = pkgs.jetbrains-mono;
+      #package = pkgs.jetbrains-mono;
     };
     themeFile = "Afterglow";
     settings = {
@@ -59,7 +58,43 @@ in
       # See https://sw.kovidgoyal.net/kitty/faq/#kitty-is-not-able-to-use-my-favorite-font
     };
     extraConfig = ''
-      symbol_map U+e000-U+e00a,U+ea60-U+ebeb,U+e0a0-U+e0c8,U+e0ca,U+e0cc-U+e0d4,U+e200-U+e2a9,U+e300-U+e3e3,U+e5fa-U+e6b1,U+e700-U+e7c5,U+f000-U+f2e0,U+f300-U+f372,U+f400-U+f532,U+f0001-U+f1af0 Symbols Nerd Font Mono
+      # https://github.com/ryanoasis/nerd-fonts/wiki/Glyph-Sets-and-Code-Points
+      symbol_map U+E5FA-U+E62B Symbols Nerd Font Mono
+      # Devicons
+      symbol_map U+e700-U+e7c5 Symbols Nerd Font Mono
+      # Font Awesome
+      symbol_map U+f000-U+f2e0 Symbols Nerd Font Mono
+      # Font Awesome Extension
+      symbol_map U+e200-U+e2a9 Symbols Nerd Font Mono
+      # Material Design Icons
+      symbol_map U+f0001-U+f1af0 Symbols Nerd Font Mono
+      # Weather
+      symbol_map U+e300-U+e3e3 Symbols Nerd Font Mono
+      # Octicons
+      symbol_map U+f400-U+f532 Symbols Nerd Font Mono
+      symbol_map U+2665 Symbols Nerd Font Mono
+      symbol_map U+26A1 Symbols Nerd Font Mono
+      # [Powerline Symbols]
+      symbol_map U+e0a0-U+e0a2 Symbols Nerd Font Mono
+      symbol_map U+e0b0-U+e0b3 Symbols Nerd Font Mono
+      # Powerline Extra Symbols
+      symbol_map U+e0b4-U+e0c8 Symbols Nerd Font Mono
+      symbol_map U+e0cc-U+e0d4 Symbols Nerd Font Mono
+      symbol_map U+e0a3 Symbols Nerd Font Mono
+      symbol_map U+e0ca Symbols Nerd Font Mono
+      # IEC Power Symbols
+      symbol_map U+23fb-U+23fe Symbols Nerd Font Mono
+      symbol_map U+2b58 Symbols Nerd Font Mono
+      # Font Logos (Formerly Font Linux)
+      symbol_map U+f300-U+f32f Symbols Nerd Font Mono
+      # Pomicons
+      symbol_map U+e000-U+e00a Symbols Nerd Font Mono
+      # Codicons
+      symbol_map U+ea60-U+ebeb Symbols Nerd Font Mono
+      # Heavy Angle Brackets
+      symbol_map U+276c-U+2771 Symbols Nerd Font Mono
+      # Box Drawing
+      symbol_map U+2500-U+259f Symbols Nerd Font Mono
     '';
   };
 
