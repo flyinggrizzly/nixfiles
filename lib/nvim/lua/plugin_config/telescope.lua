f15cd36724214f4641076d3ab226468238d0fe77
Sean DMR <sean@flyinggrizzly.net>
2024-10-13 13:17:37 +0100

    Update telescope Ctrl-P configs to handle ignored files

--- lib/nvim/lua/plugin_config/telescope.lua
+++ lib/nvim/lua/plugin_config/telescope.lua
@@ -15,9 +15,17 @@ pcall(require('telescope').load_extension, 'ui-select')
 -- See `:help telescope.builtin`
 local builtin = require 'telescope.builtin'
 
+vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Ctrl-P Search Files' })
+vim.keymap.set('n', '<C-l>', function()
+  builtin.find_files({
+    hidden = true,
+    no_ignore = true,
+    no_ignore_parent = true,
+  })
+end, { desc = 'Ctrl-P Search Files (with ignored files)' })
+
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
