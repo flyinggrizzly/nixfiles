ce35ebcb33025814c0ed536ee14d43eeb77497cd
Sean DMR <sean@flyinggrizzly.net>
2025-02-12 08:48:55 +0000

    Tweak avante config

--- lib/nvim/lua/plugin_config/avante.lua
+++ lib/nvim/lua/plugin_config/avante.lua
@@ -3,4 +3,18 @@ require('render-markdown').setup({
 })
 
 require('avante_lib').load()
-require('avante').setup()
+require('avante').setup({
+  provider = "claude",
+  auto_suggestions_provider = "claude",
+  suggestion = {
+    debounce = 1000
+  },
+  --vendors = {
+    --["local--llama3.1"] = {
+      --__inherited_from = "openai",
+      --api_key_name = "",
+      --endpoint = "http://127.0.0.1:11434/v1",
+      --model = "llama3.1:8b"
+    --},
+  --}
+})
