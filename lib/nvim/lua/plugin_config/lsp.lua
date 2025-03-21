07dc607279c7dcf3cd765c4fe4d634588e7162f0
Sean DMR <sean.moranrichards@shopify.com>
2025-03-21 13:57:43 +0000

    Remove Rubocop LSP, reprioritize Ruby LSPs

--- lib/nvim/lua/plugin_config/lsp.lua
+++ lib/nvim/lua/plugin_config/lsp.lua
@@ -172,32 +172,14 @@ local servers = {
   },
 
   ruby_lsp = {
-    root_dir = function(fname)
-      -- Don't enable the Ruby LSP if we're relying on Sorbet
-      local sorbet = sorbet_root_dir(fname)
-
-      if not sorbet then
-        lsp.util.root_pattern('Gemfile', '.ruby-lsp')(fname)
-      end
-    end,
-    priority = 100,
-  },
-
-  rubocop = {
-    cmd = { 'bundle', 'exec', 'rubocop', '--lsp' },
-    filetypes = { 'ruby' },
-    root_dir = lsp.util.root_pattern('Gemfile', '.git', 'rubocop.yml'),
-    priority = 1000,
-    on_attach = function(client, _)
-      client.server_capabilities.codeActionProvider = false
-    end,
+    priority = 1,
   },
 
   sorbet = {
     cmd = { 'bundle', 'exec', 'srb', 'tc', '--lsp' },
     filetypes = { 'ruby' },
     root_dir = sorbet_root_dir,
-    priority = 1,
+    priority = 1000000,
   },
 
 }
