df1b72954c29bee85e94ae5c47a1e567bfc7459e
Sean DMR <sean.moranrichards@shopify.com>
2025-08-28 11:22:38 +0100

    Remove codecompanion, fix claude-code name in neovim

--- lib/nvim/lua/plugin_config/llms.lua
+++ lib/nvim/lua/plugin_config/llms.lua
@@ -10,69 +10,14 @@ if not require('helpers').is_git_subprocess() then
     copilot_model = "gpt-4o-copilot",
   })
 
-  require("plugin_config/llms/codecompanion_fidget"):init()
-
-  local codecompanion = require('codecompanion')
-  codecompanion.setup({
-    strategies = {
-      chat = {
-        adapter = "sonnet_37",
-        send = {
-          modes = { n = "<C-i>", i = "<C-i>" },
-        },
-      },
-
-      inline = {
-        adapter = "copilot",
-      },
-    },
-
-    display = {
-      action_palette = {
-        provider = 'telescope',
-      },
-    },
-
-    adapters = {
-      sonnet_37 = function()
-        return require("codecompanion.adapters").extend("anthropic", {
-          name = "sonnet_37",
-          schema = {
-            model = {
-              default = "claude-3-7-sonnet-20250219",
-            },
-          },
-          env = {
-            api_key = "cmd:op read op://Personal/anthropic-api-key/password"
-          },
-        })
-      end,
-
-      deepseek_r1 = function()
-        return require("codecompanion.adapters").extend("ollama", {
-          name = "deepseek_r1",
-          schema = {
-            model = {
-              default = "deepseek-r1:1.5b",
-            },
-          },
-        })
-      end,
-    },
-  })
-
   local map = function(keys, func, desc, mode)
     mode = mode or 'n'
     vim.keymap.set(mode, keys, func, { noremap = true, silent = true, desc = 'LLM: ' .. desc })
   end
 
   if vim.fn.executable('claude') == 1 then
-    require('coder-claude-code-nvim').setup()
+    require('claudecode').setup()
   end
-
-  map('<leader>lc', '<cmd>CodeCompanionChat Toggle<cr>', 'Open [L]LM [C]hat', { 'n', 'v' })
-  map('<leader>la', '<cmd>CodeCompanionActions<cr>', 'Open [L]LM [A]ctions', { 'n', 'v' })
-  map('al', '<cmd>CodeCompanionChat Add<cr>', '[A]dd selection to [L]LM chat', 'v')
 
   map('<leader>ll', '<cmd>ClaudeCode<cr>', '[L]aunch C[L]aude Code', { 'n', 'v' })
   map('<leader>llc', '<cmd>ClaudeCode --continue<cr>', '[L]aunch C[L]aude Code, [C]ontinue last convo', { 'n', 'v' })
