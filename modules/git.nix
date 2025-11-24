6e6c4996f77ed0b96f8c62e112c3c533232a9f1a
Sean DMR <sean.moranrichards@shopify.com>
2025-11-24 16:03:10 +0000

    Update git config after deprecations

--- modules/git.nix
+++ modules/git.nix
@@ -41,8 +41,47 @@ in
     # Basic git configuration will go here
     programs.git = {
       enable = true;
-      userName = cfg.username;
-      userEmail = cfg.email;
+
+      settings = {
+        user = {
+          name = cfg.username;
+          email = cfg.email;
+        };
+
+        alias = {
+          l = "log --pretty-colored";
+          st = "status";
+          d = "diff";
+          dd = "difftool";
+          hist = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+          branches = "for-each-ref --sort=-committerdate --format=\"%(color:blue)%(authordate:relative)\t%(color:red)%(authorname)\t%(color:white)%(color:bold)%(refname:short)\" refs/remotes";
+          co = "checkout";
+          cp = "cherry-pick";
+          po = "!git push -u origin \"$(git rev-parse --abbrev-ref HEAD)\"";
+          cane = "commit --amend --no-edit";
+          # git add -p will also interact with untracked files
+          ap = "!git add --intent-to-add && git add -p";
+          hide = "update-index --skip-worktree";
+          unhide = "update-index --no-skip-worktree";
+          squash = "rebase -i --autosquash";
+        };
+
+        pull.rebase = true;
+        init.defaultBranch = "main";
+        rebase.autosquash = true;
+        push.default = "current";
+        commit.verbose = true;
+        diff.tool = "vimdiff";
+        absorb.maxStack = 50;
+
+        # Add any custom git config
+        core.editor = "nvim";
+        diff.algorithm = "patience";
+        merge.conflictstyle = "diff3";
+
+        # Merge user's extra config
+      }
+      // cfg.extraConfig;
 
       ignores = [
         "*.pyc"
@@ -65,40 +104,6 @@ in
         "/.direnv/"
       ];
 
-      aliases = {
-        l = "log --pretty-colored";
-        st = "status";
-        d = "diff";
-        dd = "difftool";
-        hist = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-        branches = "for-each-ref --sort=-committerdate --format=\"%(color:blue)%(authordate:relative)\t%(color:red)%(authorname)\t%(color:white)%(color:bold)%(refname:short)\" refs/remotes";
-        co = "checkout";
-        cp = "cherry-pick";
-        po = "!git push -u origin \"$(git rev-parse --abbrev-ref HEAD)\"";
-        cane = "commit --amend --no-edit";
-        # git add -p will also interact with untracked files
-        ap = "!git add --intent-to-add && git add -p";
-        hide = "update-index --skip-worktree";
-        unhide = "update-index --no-skip-worktree";
-        squash = "rebase -i --autosquash";
-      };
-
-      extraConfig = {
-        pull.rebase = true;
-        init.defaultBranch = "main";
-        rebase.autosquash = true;
-        push.default = "current";
-        commit.verbose = true;
-        diff.tool = "vimdiff";
-        absorb.maxStack = 50;
-
-        # Add any custom git config
-        core.editor = "nvim";
-        diff.algorithm = "patience";
-        merge.conflictstyle = "diff3";
-
-        # Merge user's extra config
-      } // cfg.extraConfig;
     };
 
     home.packages = [
