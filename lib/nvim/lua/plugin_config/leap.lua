1c2462bfcc3001b580a65fa7a4a9b7f1a40f0605
Sean DMR <sean@flyinggrizzly.net>
2026-01-23 21:44:20 +0000

    Update leap config

--- lib/nvim/lua/plugin_config/leap.lua
+++ lib/nvim/lua/plugin_config/leap.lua
@@ -1,1 +1,10 @@
-require('leap').create_default_mappings()
+local leap = require('leap')
+
+local function do_leap(args)
+  return function()
+    leap.leap(args)
+  end
+end
+
+vim.keymap.set({'n', 'x', 'o'}, 's',  do_leap())
+vim.keymap.set({'n', 'x', 'o'}, 'S',  do_leap({ backward = true }))
