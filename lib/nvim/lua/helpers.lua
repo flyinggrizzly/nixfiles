e5bd9301c8de396294ce9ffa590a3e018f0a60d3
Sean DMR <sean@flyinggrizzly.net>
2026-01-24 09:14:42 +0000

    Update treesitter to new main branch
    Ref: https://github.com/nvim-treesitter/nvim-treesitter/discussions/7927#discussioncomment-14479019

--- lib/nvim/lua/helpers.lua
+++ lib/nvim/lua/helpers.lua
@@ -1,4 +1,5 @@
 return {
   is_git_subprocess = require('helpers/is_git_subprocess'),
   llms_enabled = require('helpers/llms_enabled'),
+  table_except = require('helpers/table_except'),
 }
--- lib/nvim/lua/plugin_config/treesitter.lua
+++ lib/nvim/lua/plugin_config/treesitter.lua
@@ -1,13 +1,55 @@
-local textobjects = require('plugin_config/treesitter/textobjects')
+local treesitter = require('nvim-treesitter')
 
-require('nvim-treesitter.configs').setup({
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = { 'ruby' },
-  },
-  indent = {
-    enable = true,
-    disable = { 'ruby' },
-  },
-  textobjects = textobjects.map().configure(),
-})
+treesitter.setup {}
+local should_install = {
+  'vim',
+  'nix',
+  'xml',
+  'css',
+  'bash',
+  'diff',
+  'lua',
+  'luap',
+  'luadoc',
+  'vimdoc',
+  'typescript',
+  'javascript',
+  'jsdoc',
+  'html',
+  'http',
+  'json',
+  'sql',
+  'ruby',
+  'csv',
+  'gitignore',
+  'gitcommit',
+  'gitattributes',
+  'git_config',
+  'rust',
+  'query',
+  'toml',
+  'yaml',
+  'regex',
+  'markdown',
+  'markdown_inline',
+}
+
+treesitter.install(
+  require('helpers').table_except(should_install, treesitter.get_installed())
+)
+
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function(args)
+    if
+      vim.list_contains(
+        treesitter.get_installed(),
+        vim.treesitter.language.get_lang(args.match)
+      )
+      then
+        vim.treesitter.start(args.buf)
+      end
+    end,
+  }
+)
+
+require('plugin_config/treesitter/textobjects')
